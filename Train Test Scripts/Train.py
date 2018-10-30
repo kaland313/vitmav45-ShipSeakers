2293b861ade1528ca05c50bdcbbf3d9d6ae3f33b
András Kalapos <kalapos.andras@gmail.com>
2018-10-30 22:31:41 +0100

    Early stopping and model checkpoint (auto saving) added + cleanup

--- Train Test Scripts/Train.py
+++ Train Test Scripts/Train.py
@@ -1,36 +1,53 @@
+########################################################################################################################
+# Imports
+########################################################################################################################
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+from matplotlib.colors import ListedColormap
 from tqdm import tqdm
-
 import imageio
-import sklearn
 import cv2
 
 from keras.applications import imagenet_utils
 from keras.utils import Sequence
+from keras import backend as K
+from keras.utils import plot_model
 
-import matplotlib.pylab as pl
-from matplotlib.colors import ListedColormap
+import keras.models as models
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape, Permute
+from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# import json
+# import tensorflow as tf
+# from keras.backend.tensorflow_backend import set_session
+# from tensorflow.python.client import device_lib
+
+########################################################################################################################
+# GPU info
+########################################################################################################################
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True
+# set_session(tf.Session(config=config))
+# Device check
+# print(device_lib.list_local_devices())
+
+
+print(K.tensorflow_backend._get_available_gpus())
+
+########################################################################################################################
+# Function definitions
+########################################################################################################################
 cmap = pl.cm.viridis
 my_cmap = cmap(np.arange(cmap.N))
 my_cmap[:,-1] = np.linspace(0, 1, cmap.N)
 my_cmap = ListedColormap(my_cmap)
-
-
-# import json
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-from tensorflow.python.client import device_lib
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-set_session(tf.Session(config=config))
-# Device check
-# print(device_lib.list_local_devices())
-
-from keras import backend as K
-print(K.tensorflow_backend._get_available_gpus())
-
 
 def preprocess_input(x):
     """Preprocesses a Numpy array encoding a batch of images.
@@ -163,60 +180,55 @@ class DataGenerator(Sequence):
 
         # Generate data
         for i, ID in enumerate(tmp_list):
-            # read image called "ID"
-            X[i] = read_transform_image(self.img_prefix + "/" + self.ship_seg_df.at[ID, 'ImageId'])
-            # X[i] = cv2.resize(read_transform_image(self.img_prefix + "/" + self.ship_seg_df.at[ID, 'ImageId']), (192, 192))
-            # print("\n\n{}\n\n".format(X[i]))
-
-            # store masks
+            # read image and mask referred by "ID"
             mask = rle_decode(self.ship_seg_df.at[ID, 'EncodedPixels'])
-            Y[i] = np.atleast_3d(mask)
-            # Y[i] = np.atleast_3d(cv2.resize(mask, (192, 192), interpolation=cv2.INTER_NEAREST))
+            if self.dim == (768, 768):
+                X[i] = read_transform_image(self.img_prefix + "/" + self.ship_seg_df.at[ID, 'ImageId'])
+                Y[i] = np.atleast_3d(mask)
+            else:
+                X[i] = cv2.resize(read_transform_image(self.img_prefix + "/" + self.ship_seg_df.at[ID, 'ImageId']), *self.dim)
+                Y[i] = np.atleast_3d(cv2.resize(mask, *self.dim, interpolation=cv2.INTER_NEAREST))
 
         return X, Y
 
 
 ########################################################################################################################
+# PARAMETERS
 ########################################################################################################################
-#Load the file which contains the masks for each image
-df_train = pd.read_csv('../data/train_ship_segmentations_v2.csv')
-
-########################################################################################################################
-import os
-img_files = os.listdir('../data/train_img')
-df_train['img_found'] = False
-for img_file in img_files:
-    df_train['img_found'] = (df_train['img_found']) | (df_train['ImageId']==img_file)
-df_train = df_train[df_train['img_found']]
-df_train = df_train.drop('img_found',axis = 1)
-df_train = df_train.reset_index(drop=True)
-########################################################################################################################
-
-#Testing split
+image_path = "../data/train_img"
+segmentation_data_file_path = '../data/train_ship_segmentations_v2.csv'
 valid_split = 0.15
 test_split = 0.15
 
-train_img_ids, valid_img_ids, test_img_ids = separate(df_train.index.values, valid_split, test_split)
-
-# dictionaries for the generators
-partitions = {"train": train_img_ids, "validation": valid_img_ids, "test":test_img_ids}
-# # {"imgID1" : rle mask1, "imgID2" : rle_mask1, ... }
-# labels = dict()
-#
-# for i in tqdm(range(df_train["ImageId"].values.shape[0])):
-#     labels[df_train["ImageId"].values[i]] = df_train.loc[df_train.ImageId == df_train["ImageId"].values[i]].EncodedPixels.values
-
-
-image_path = "../data/train_img"
-img_shape = read_transform_image(image_path + "/"+ df_train.at[0,'ImageId']).shape
-print("RGB channels: ", img_shape[2])
-print("Image size:", img_shape[0:2])
-rgb_channels_number = img_shape[2] #3
-dimension_of_the_image = img_shape[0:2] #(768. 768) (192,192)#
+resize_img_to = (768, 768) #(196,196)
 batch_size = 1
 
+########################################################################################################################
+# Load and prepare the data
+########################################################################################################################
+
+# Load the file which contains the masks for each image
+df_train = pd.read_csv(segmentation_data_file_path)
+
+# Look for missing files and remove them from the dataframe
+# import os
+# img_files = os.listdir('../data/train_img')
+# df_train['img_found'] = False
+# for img_file in img_files:
+#     df_train['img_found'] = (df_train['img_found']) | (df_train['ImageId']==img_file)
+# df_train = df_train[df_train['img_found']]
+# df_train = df_train.drop('img_found',axis = 1)
+# df_train = df_train.reset_index(drop=True)
+
+# Split the data
+train_img_ids, valid_img_ids, test_img_ids = separate(df_train.index.values, valid_split, test_split)
+
+# Define the generators
+rgb_channels_number = 3
+dimension_of_the_image = resize_img_to
+
 training_generator = DataGenerator(
-    partitions["train"],
+    train_img_ids,
     df_train,
     image_path,
     batch_size=batch_size,
@@ -225,7 +237,7 @@ training_generator = DataGenerator(
 )
 
 validation_generator = DataGenerator(
-    partitions["validation"],
+    valid_img_ids,
     df_train,
     image_path,
     batch_size=batch_size,
@@ -233,70 +245,65 @@ validation_generator = DataGenerator(
     n_channels=rgb_channels_number
 )
 
-import time
-start = time.time()
-for idx in range(100):
-    gen_img, gen_mask = training_generator.__getitem__(10)
-stop = time.time()
-print("Generation time:", stop-start)
-
-
+# Test the generators
+# gen_img, gen_mask = training_generator.__getitem__(10)
 # print(gen_img.shape, gen_mask.shape)
 # disp_image_with_map(gen_img[0], gen_mask[0])
 
 
 ########################################################################################################################
-import keras.models as models
-from keras.models import Model
-from keras.layers import Input
-from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape, Permute
-from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Concatenate
+# Build the network
+########################################################################################################################
 
-
-def create_encoding_layers(input_layer):
+def create_encoding_layers_residual(input_layer):
     kernel = 3
     filter_size = 64
     pool_size = 2
+    residual_connections = []
 
     x = Conv2D(filter_size, kernel, padding='same')(input_layer)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(pool_size, pool_size))(x)
+    residual_connections.append(x)
 
     x = Conv2D(128, kernel, padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(pool_size, pool_size))(x)
+    residual_connections.append(x)
 
     x = Conv2D(256, kernel, padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(pool_size, pool_size))(x)
+    residual_connections.append(x)
 
     x = Conv2D(512, kernel, padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    return x
+    return x, residual_connections
 
 
-def create_decoding_layers(input_layer):
+def create_decoding_layers_residual(input_layer, residual_connections):
     kernel = 3
     filter_size = 64
     pool_size = 2
 
     x = Conv2D(512, kernel, padding='same')(input_layer)
     x = BatchNormalization()(x)
+    x = Concatenate()([x, residual_connections[2]])
 
     x = UpSampling2D(size=(pool_size, pool_size))(x)
     x = Conv2D(256, kernel, padding='same')(x)
     x = BatchNormalization()(x)
+    x = Concatenate()([x, residual_connections[1]])
 
     x = UpSampling2D(size=(pool_size, pool_size))(x)
     x = Conv2D(128, kernel, padding='same')(x)
     x = BatchNormalization()(x)
+    x = Concatenate()([x, residual_connections[0]])
 
     x = UpSampling2D(size=(pool_size, pool_size))(x)
     x = Conv2D(filter_size, kernel, padding='same')(x)
@@ -304,38 +311,47 @@ def create_decoding_layers(input_layer):
 
     return x
 
-
 ########################################################################################################################
 classes = 1
 
 input_layer = Input((*dimension_of_the_image, 3))
 
-encoded_layer = create_encoding_layers(input_layer)
-decoded_layer = create_decoding_layers(encoded_layer)
+encoded_layer, residual_conns = create_encoding_layers_residual(input_layer)
+decoded_layer = create_decoding_layers_residual(encoded_layer, residual_conns)
 
 final_layer = Conv2D(classes, 1, padding='same')(decoded_layer)
 final_layer = Activation('softmax')(final_layer)
 
-semseg_model_residual = Model(inputs=input_layer, outputs=final_layer)
+model = Model(inputs=input_layer, outputs=final_layer)
 
-semseg_model_residual.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-print(semseg_model_residual.summary())
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+print(model.summary())
 
-from keras.utils import plot_model
-plot_model(semseg_model_residual, to_file='model.png',show_shapes=True)
-#######################################################################################################################
-semseg_model_residual.fit_generator(generator=training_generator,
-                           steps_per_epoch=len(training_generator),
-                           epochs=100, validation_data=validation_generator,
-                           validation_steps=len(validation_generator), verbose=1)
+plot_model(model, to_file='model.png', show_shapes=True)
 
+########################################################################################################################
+# Train the network
+########################################################################################################################
+patience=40
+early_stopping=EarlyStopping(patience=patience, verbose=1)
+checkpointer=ModelCheckpoint(filepath='fully_connected_weights.hdf5', save_best_only=True, verbose=1)
+model.fit_generator(generator=training_generator,
+                    steps_per_epoch=len(training_generator),
+                    epochs=100,
+                    validation_data=validation_generator,
+                    validation_steps=len(validation_generator),
+                    callbacks=[checkpointer, early_stopping],
+                    verbose=1)
+
+
+########################################################################################################################
+# Test the network
+########################################################################################################################
 # test_generator = DataGenerator(
-#     partitions["test"],
+#     test_img_ids,
 #     df_train,
-#     "../data/test_img",
+#     image_path,
 #     batch_size=batch_size,
 #     dim=dimension_of_the_image,
 #     n_channels=rgb_channels_number
 # )
-
-# semseg_model_residual.save('semseg_model.hf5')
