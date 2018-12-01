287aa6338fb290044fa3a9c331933cd7b4f8d000
András Kalapos <kalapos.andras@gmail.com>
2018-12-01 19:36:54 +0100

    Drop corrupted images from the dataset

--- Train Test Scripts/Train.py
+++ Train Test Scripts/Train.py
@@ -81,6 +81,15 @@ df_train = pd.read_csv(segmentation_data_file_path)
 # df_train = df_train.drop('img_found', axis=1)
 # df_train = df_train.reset_index(drop=True)
 
+# Drop corrupted images
+# List source: https://www.kaggle.com/iafoss/unet34-dice-0-87
+exclude_list = ['6384c3e78.jpg', '13703f040.jpg', '14715c06d.jpg', '33e0ff2d5.jpg',
+                '4d4e09f2a.jpg', '877691df8.jpg', '8b909bb20.jpg', 'a8d99130e.jpg',
+                'ad55c3143.jpg', 'c8260c541.jpg', 'd6c7f17c7.jpg', 'dc3e7c901.jpg',
+                'e44dffe88.jpg', 'ef87bad36.jpg', 'f083256d8.jpg'] #corrupted images
+for imgId in exclude_list:
+    df_train = df_train[~(df_train['ImageId'] == imgId)]
+
 # Split the data
 train_img_ids, valid_img_ids, test_img_ids = separate(df_train['ImageId'].values, valid_split, test_split)
 np.save("test_img_ids.npy", test_img_ids)
